f246e3998e827101fac2a5ab5699964357116615
msb741852 <msb741852@naver.com>
2023-09-15 02:39:23 +0900

    [ADD, MODIFED] 메뉴 이미지 슬라이더 구현, 비디오 섹션 크기 수정, 심플메뉴 호버 시 색상, 메뉴 이미지 크기 변경, 스토어 이미지 사이즈 변경

--- script/main.js
+++ script/main.js
@@ -8,8 +8,6 @@ $(document).ready(() => {
 
     // scroll_top이 header보다 내려가면 header fixed
     if (header_height <= scroll_top) {
-      console.log(video_section_top_gap);
-
       $(".header").addClass("header_active");
       $(".container").css({
         paddingTop: header_height + "px",
@@ -27,4 +25,81 @@ $(document).ready(() => {
       $(".slogun_top_txt, .slogun_bottom_txt").addClass("slogun_active");
     }
   });
+
+  //==================== menu slide ================================
+
+  let menu_item_width = $(".menu_item").width();
+  for (let i = 0; i < $(".menu_item").length; i++) {
+    $(".menu_item")
+      .eq(i)
+      .css({
+        left: menu_item_width * i,
+      });
+  }
+  let curr_slide_no = 0;
+  let menu_item_cnt = $(".menu_item").length;
+  let slide_timer = 1000;
+  let slide_interval;
+  $(".btn_next").click(() => {
+    prevent_btn(slide_timer);
+    slide_all(`-=${menu_item_width}`, slide_timer);
+    slide_init(
+      curr_slide_no % menu_item_cnt,
+      menu_item_width * ($(".menu_item").length - 1)
+    );
+    curr_slide_no += 1;
+  });
+  $(".btn_prev").click(() => {
+    prevent_btn(slide_timer);
+    // 현재 슬라이드 번호 -1 left: -width
+    slide_init((curr_slide_no - 1) % menu_item_cnt, `-${menu_item_width}`);
+    // item 전체 left +넓이만큼
+    slide_all(`+=${menu_item_width}`, slide_timer);
+    curr_slide_no -= 1;
+  });
+
+  auto_slide();
+  $(".menu_list").hover(
+    () => {
+      clearInterval(slide_interval);
+    },
+    () => {
+      auto_slide();
+    }
+  );
+
+  () => {};
+  function slide_all(left_value, timer) {
+    $(".menu_item").animate(
+      {
+        left: left_value,
+      },
+      timer,
+      "linear"
+    );
+  }
+  function slide_init(eq_value, left_value) {
+    $(".menu_item").eq(eq_value).animate(
+      {
+        left: left_value,
+      },
+      0,
+      "linear"
+    );
+  }
+  function prevent_btn(timer) {
+    $(".slide_btn").css({
+      pointerEvents: "none",
+    });
+    setTimeout(() => {
+      $(".slide_btn").css({
+        pointerEvents: "auto",
+      });
+    }, timer);
+  }
+  function auto_slide() {
+    slide_interval = setInterval(() => {
+      $(".btn_next").trigger("click");
+    }, slide_timer + 1000);
+  }
 });
