572027340f6aff842276b12daf12059b57a79349
msb741852 <msb741852@naver.com>
2023-09-17 02:08:10 +0900

    [ADD] 헤더 메뉴 호버시 해당 상세메뉴 내려오도록 구현

--- script/main.js
+++ script/main.js
@@ -1,6 +1,42 @@
 $(document).ready(() => {
   let header_height = $(".header").innerHeight();
   let video_section_top_gap = $(".video_section").offset().top;
+  let curr_detail_menu_height;
+  let curr_detail_idx;
+  $(".navbar_item").hover(
+    function () {
+      $(".detail_menu").removeClass("detail_menu_active");
+      // 현재 마우스 엔터된 요소의 자식 중 클래스가 detail_memnu인 요소가 있다면 높이값 저장 아니면 0
+      curr_detail_menu_height =
+        $(this).children(".detail_menu").length == 0
+          ? 0
+          : $(this).children(".detail_menu").innerHeight();
+      if (curr_detail_menu_height != 0) {
+        $(".detail_menu_board").css({
+          height: curr_detail_menu_height,
+        });
+      }
+      $(this).children(".detail_menu").addClass("detail_menu_active");
+      curr_detail_idx = $(this).index();
+    },
+    function () {
+      $(".detail_menu_board").css({
+        height: 0,
+      });
+      $(".detail_menu").removeClass("detail_menu_active");
+    }
+  );
+  $(".detail_menu_board").hover(
+    function () {
+      $(".navbar_item").eq(curr_detail_idx).trigger("mouseenter");
+    },
+    function () {
+      $(".detail_menu_board").css({
+        height: 0,
+      });
+      $(".detail_menu").removeClass("detail_menu_active");
+    }
+  );
 
   $(document).scroll(() => {
     let scroll_top = $(window).scrollTop();
